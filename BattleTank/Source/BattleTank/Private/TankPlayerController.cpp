8beeeaf9510ef1e6e5222949ee8a7c1993dfa3d8
Amin Latifkar <amin.latifkar@gmail.com>
2019-06-24 19:15:02 +0430

    BT25 Add Tick() to PlayerController.cmproj

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -18,9 +18,19 @@ void ATankPlayerController::BeginPlay()
 	}
 }
 
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	AimTowardCrosshair();
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
 
-
+void ATankPlayerController::AimTowardCrosshair() 
+{
+	if (!GetControlledTank()) { return;  }
+}
