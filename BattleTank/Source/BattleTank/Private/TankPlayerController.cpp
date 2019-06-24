ba2c8332c1b042d98910c69e177ec0ed8a34d939
Amin Latifkar <amin.latifkar@gmail.com>
2019-06-24 17:18:09 +0430

    BT22 Using virtual and override.cmproj

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -3,7 +3,24 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ATank* ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No tanks possessed"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Possesed Tank: %s"), *ControlledTank->GetName());
+	}
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
+
+
