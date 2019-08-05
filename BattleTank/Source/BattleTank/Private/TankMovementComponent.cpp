eb3e5f73e7715f8d3a54fc7fdfab64b2409d1d3f
Amin Latifkar <amin.latifkar@gmail.com>
2019-08-05 15:14:14 +0430

    BT58 A Better Component Architecture

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,10 +1,21 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
+#include "TankTrack.h"
 #include "TankMovementComponent.h"
 
-void UTankMovementComponent::IntendMoveForeward(float Throw) 
+void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+	this->LeftTrack = LeftTrackToSet;
+	this->RightTrack = RightTrackToSet;
+}
+
+void UTankMovementComponent::IntendMoveForeward(float Throw)
 {
 	//float time = GetWorld()->GetTimeSeconds();
 	UE_LOG(LogTemp, Warning, TEXT("Intend move foreward: %f"), Throw);
+
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
 }
--- BattleTank/Source/BattleTank/Public/TankMovementComponent.h
+++ BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -6,15 +6,22 @@
 #include "GameFramework/NavMovementComponent.h"
 #include "TankMovementComponent.generated.h"
 
+class UTankTrack;
+
 /**
- *
+ * Responsible for driving the tank tracks
  */
-UCLASS()
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
 {
 	GENERATED_BODY()
 
 public:
+	UFUNCTION(BlueprintCallable, Category = "Setup")
+		void Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSe);
 	UFUNCTION(BlueprintCallable, Category = "Input")
 		void IntendMoveForeward(float Throw);
+private:
+	UTankTrack* LeftTrack = nullptr;
+	UTankTrack* RightTrack = nullptr;
 };
